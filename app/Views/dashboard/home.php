8fdbf1a40126de26096ef388b1759dd0daed8a3a
Gustavo Storb <ocanaldotroll@gmail.com>
2022-05-12 00:41:15 -0300

    fixes

--- app/Views/dashboard/home.php
+++ app/Views/dashboard/home.php
@@ -8,32 +8,33 @@ if(!defined('2022T2')){
 <div class="main-wrapper">
 <?php include('./app/Layouts/header.php'); ?>
 <title>Projeto Integrador - Painel</title>
-
-    <section class="dashboard-card mt-80">
-        <div>
-            <h1>Perfil do usuário<h1>
-            <h5>ID: <?php echo $_SESSION['usuario_id'] ?></h5>
-            <h5>Nome: <?php echo $_SESSION['usuario_nome'] ?></h5>
-            <h5>E-mail: <?php echo $_SESSION['usuario_email'] ?></h5>
-            <h5>Cargo: <?php echo $_SESSION['perfil'] === 'adm' ? 'Administrador' : 'Funcionário' ?></h5>
-            <h5>Habilitado: <?php echo $_SESSION['licensed'] === 1 ? 'Sim' : 'Não' ?></h5>
-            </div>
-            <div>
-                <h1 class="text-center">AÇÕES</h1>
-                <div class="actions-buttons-table">
-                    <button class="mr-16 action">Cadastrar Chamado</button>
-                    <button class="action">Consultar Chamado</button>
-                </div>
-                <div class="actions-buttons-table">
-                    <button class="action">Cadastrar Veiculo</button>
-                    <button class="action">Consultar Veiculo</button>
-                </div>
-                <div class="actions-buttons-table">
-                    <a href="./cadastrar">
-                        <button class="action">Cadastrar Funcionário</button>
-                    </a>
-                    <button class="action">Consultar Funcionário</button>
-                </div>
-            </div>
-    </section>
- </div>
+<section class="dashboard-card mt-80">
+   <div>
+      <h1>
+      Perfil do usuário
+      <h1>
+      <h5>ID: <?php echo $_SESSION['usuario_id'] ?></h5>
+      <h5>Nome: <?php echo $_SESSION['usuario_nome'] ?></h5>
+      <h5>E-mail: <?php echo $_SESSION['usuario_email'] ?></h5>
+      <h5>Cargo: <?php echo $_SESSION['perfil'] === 'adm' ? 'Administrador' : 'Funcionário' ?></h5>
+      <h5>Habilitado: <?php echo $_SESSION['licensed'] === 1 ? 'Sim' : 'Não' ?></h5>
+   </div>
+   <div class="actions-dashboard-buttons">
+      <h1 class="text-center">AÇÕES</h1>
+      <div class="actions-buttons-table">
+         <button class="mr-16 action">Cadastrar Chamado</button>
+         <button class="action">Consultar Chamado</button>
+      </div>
+      <div class="actions-buttons-table">
+         <button class="action">Cadastrar Veiculo</button>
+         <button class="action">Consultar Veiculo</button>
+      </div>
+      <div class="actions-buttons-table">
+         <a href="./cadastrar">
+         <button class="action">Cadastrar Usuário</button>
+         </a>
+         <button class="action">Consultar Usuário</button>
+      </div>
+   </div>
+</section>
+</div>
