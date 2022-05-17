9246f3a70eef0c0dfe988ba6788237abc195767e
Gustavo Storb <ocanaldotroll@gmail.com>
2022-05-17 01:30:29 -0300

    adicionado front end pra consulta de usuarios

--- app/Controllers/Consultar.php
+++ app/Controllers/Consultar.php
@@ -30,18 +30,70 @@ class Consultar
 
             $stmt = $connect->prepare($sql);
             $stmt->execute(); 
-            $user = $stmt->fetch();
+            $result = $stmt->fetchAll();
             
             $carregarView = new \Core\ConfigView("Views/usuario/consultar", $this->dados);
             $carregarView->renderizar();
 
-            $cod = $user[0];
-            $nome = $user[1];
-            $email = $user[2];
-            $tipo = $user[3];
-            $licensed = $user[4];
+            ?> 
+            <html>
+            <table class="tabela">
+                    <tr>
+                        <th>#</th>   
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Cargo</th>
+                        <th>Habilitado</th>
+                        <th>Ações</th>
 
-            echo $nome;
+                        <?php 
+                        if($_SESSION['perfil'] == 'adm'){
+                        ?>
+                        <?php } ?>
+
+                    <?php foreach($result as &$valor): ?>
+                    </tr>
+
+                    <tr>
+                    <td>
+                        <?php echo $valor[0];?>
+                    </td>
+                    <td>
+                        <?php echo $valor[1];?>
+                    </td>
+                    <td>
+                        <?php echo $valor[2]?>
+                    </td>
+                    <td>
+                    <?php
+                        $tipo = $valor[4];
+                            if($tipo == 'adm'){
+                                echo "Administrador";
+                            }elseif($tipo == 'user'){
+                                echo "Funcionário";
+                            }
+                        ?>
+                    </td>
+                    <td>
+                    <?php 
+                        $licensed = $valor[5];
+                            if($licensed == '1'){
+                                echo "Sim";
+                            }elseif($licensed == '0'){
+                                echo "Não";
+                            }
+                        ?>
+
+                    </td>
+                    <td>
+                        <button><i style="font-size:18px" class="fa fa-pencil"></i>
+                        <button><i style="font-size:18px" class="fa fa-trash"></i>
+                    </td>
+                    <?php endforeach; ?>
+                        </tr>
+
+            </html>
+            <?php
         } catch (\PDOException $e){
             $carregarView = new \Core\ConfigView("Views/usuario/consultar", $this->dados);
             $carregarView->renderizar();
